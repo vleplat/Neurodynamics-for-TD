5e19b178e186f161704d8bbcc1985ff4f8fe5e6c
vleplat <valentin.leplat@gmail.com>
2023-06-19 09:21:26 +0300

    Update addpaths.m

--- CPD_CNO/addpaths.m
+++ CPD_CNO/addpaths.m
@@ -12,7 +12,13 @@ addpath("libraries/tensor_toolbox-v3.4/");
 % Tensorbox lib.
 addpath("libraries/TENSORBOX/");
 
+addpath("libraries/NN_Comp_Toolbox/NN_Comp_Toolbox/")
+
+addpath("libraries/NN_Comp_Toolbox_2/NN_Comp_Toolbox/")
+
+addpath("libraries/NNtensorPackage_mfiles/NNtensorPackage_mfiles/")
+
 % Util functions
 addpath("Utils/");
 addpath("Datasets/");
-addpath(genpath(pwd))
+% addpath(genpath(pwd))
